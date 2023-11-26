27d4c3a2a131bdb8bc55159da60895ad11d8a331
harryitc <cuongharryit@gmail.com>
2023-11-26 00:33:30 +0000

    add authentication components

---
+++ web/src/app/authentication/changepass/changepass.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-changepass',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './changepass.component.html',
+  styleUrl: './changepass.component.scss'
+})
+export class ChangepassComponent {
+
+}
